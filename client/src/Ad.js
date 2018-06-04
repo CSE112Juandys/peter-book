e0375ed163108cc6f136380efa4aad9b0f6b74f4
garyzhao63 <garyzhao63@gmail.com>
2018-06-03 22:02:09 -0700

    Ad component

---
+++ client/src/Ad.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Ad = () => {
+    return (
+        <div className = "ad">
+            <script data-cfasync='false' type='text/javascript' src='//p290496.clksite.com/adServe/banners?tid=290496_563979_10'></script>
+            <p> HERE </p>
+        </div>
+    )
+}
+
+export default Ad
