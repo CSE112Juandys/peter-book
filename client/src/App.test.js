79eef4c90b96d9a0927b162511b57a8f0621dabd
garyzhao63 <garyzhao63@gmail.com>
2018-06-03 18:33:21 -0700

    added enzyme for testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {configure, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure ({adapter: new Adapter()})
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App/>', ()=>{
+  it('renders please', () => {
+    let wrapper = shallow(<App/>)
+    /*eslint-disable */
+    expect(wrapper.find('.App'))
+    /*eslint-enable */
+  });
 });
